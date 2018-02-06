0b4d0d564194aedaa5079a07fdd1ebd514a22412
savaughn <svaughn57@gmail.com>
2018-02-06 10:38:02 -0600

    initial commit

---
+++ ChineseRemThm.java
@@ -0,0 +1,20 @@
+class ChineseRemThm{
+
+	void run(int a, int b, int c, int d){
+		int[] cons = {a,c};
+		int[] mods = {b,d};
+		
+	}
+	
+	void solve(int[] cons, int[] mods){
+		
+	}
+
+	public static void main(Strings args[]){
+		ChineseRemThm one = new ChineseRemThm();
+		ChineseRemThm two = new ChineseRemThm();
+		ChineseRemThm three = new ChineseRemThm();
+		ChineseRemThm four = new ChineseRemThm();
+		ChineseRemThm five = new ChineseRemThm();
+	}
+}
